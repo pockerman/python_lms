d242fcdc51adab6990e0256a112801cf49569530
David Hilbert <a.giavaras@gmail.com>
2017-06-29 10:41:36 +0300

    Removed link for privacy from footer.html and urls

--- ustdy_app/urls.py
+++ ustdy_app/urls.py
@@ -3,21 +3,21 @@ from . import views
 
 urlpatterns = [
 	# post views
-	url(r'^$', 
-			views.ustdy_index, 
+	url(r'^$',
+			views.ustdy_index,
 			name='ustdy_index'),
 
-	url(r'^privacy/$',
-			views.privacy_policy_view,
-			name='privacy_policy_view'),
+	#url(r'^privacy/$',
+	#		views.privacy_policy_view,
+	#		name='privacy_policy_view'),
 
 	url(r'^about/$',
 			views.about_view,
 			name='about_view'),
 
-	url(r'^jobs/$',
-			views.jobs_view,
-			name='jobs_view'),
+	#url(r'^jobs/$',
+	#		views.jobs_view,
+	#		name='jobs_view'),
 
 	url(r'^services/development/$',
 			views.development_view,
@@ -27,5 +27,5 @@ urlpatterns = [
 			views.host_your_courses_view,
 			name='host_your_courses_view'),
 
-	
+
 ]
